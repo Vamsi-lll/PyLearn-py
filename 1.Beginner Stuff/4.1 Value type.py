2f956ff8a6226ca28094124245e4b9e052d6eef7
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2026-01-28 10:50:44 +0530

    Create 4.1 Value type.py with value type explanation
    Add initial content explaining value types and numbered types in Python.

---
+++ 1.Beginner Stuff/4.1 Value type.py
@@ -0,0 +1,13 @@
+# Vale Type -Data type
+# as the name it self saying the vlaue type this value type data types will hold a single value at a time
+# in value type as we see in the previous file ther are two seperate types 
+
+
+# 1.Numbered Type 
+# what are the numbered type ?
+# simple the data which contain only numbers are called numbered type 
+# the numbered type in the python is further classified into as follow 
+
+# int type 
+
+
