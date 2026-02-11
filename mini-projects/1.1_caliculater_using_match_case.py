c1819d27a226b472c1536e066bba400067271d71
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2026-02-11 14:39:06 +0530

    Implement calculator using match-case structure

--- mini-projects/1.1_caliculater_using_match_case.py
+++ mini-projects/1.1_caliculater_using_match_case.py
@@ -1,1 +1,25 @@
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
 
+print("\nChoose an operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+option = int(input("Enter your option: "))
+
+match option:
+    case 1:
+        print("Addition =", num1 + num2)
+    case 2:
+        print("Subtraction =", num1 - num2)
+    case 3:
+        print("Multiplication =", num1 * num2)
+    case 4:
+        if num2 == 0:
+            print("Error: Division by zero is not allowed")
+        else:
+            print("Division =", num1 / num2)
+    case _:
+        print("Invalid option selected")
